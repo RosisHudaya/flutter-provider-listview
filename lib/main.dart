914e5483482e9db189561199c469354dafb19d48
RosisHudaya <rosishudayaputra02@gmail.com>
2022-11-02 15:47:06 +0700

    SetUp

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_listview/service/tasklist.dart';
+
+import 'page/addtask.dart';
+import 'page/listpage.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Tasklist>(
+          create: (context) => Tasklist(),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/": (context) => const MyListPage(),
+        "/addTask": (context) => AddTaskPage(),
+      },
+      initialRoute: "/",
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
